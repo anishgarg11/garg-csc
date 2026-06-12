01025f1c366dd4a6477968cf70980062ec09f1d6
anishgarg11 <garganish55@gmail.com>
2026-06-12 19:12:23 +0530

    Udate footer

--- garg-csc/src/components/Footer.jsx
+++ garg-csc/src/components/Footer.jsx
@@ -5,29 +5,36 @@ export default function Footer() {
   return (
     <footer className="footer">
       <h4>🏪 Garg Common Service Center</h4>
-      <p>Faridabad, Haryana &nbsp;|&nbsp; Operating under the Digital India Mission</p>
+
+      <p>
+        Faridabad, Haryana | Operating under the Digital India Mission
+      </p>
 
       <div className="footer-links">
         <Link to="/services">Services</Link>
         <Link to="/about">About Us</Link>
         <Link to="/certificate">Certificate</Link>
         <Link to="/contact">Contact</Link>
-        <a href="https://csc.gov.in" target="_blank" rel="noreferrer">CSC India Portal</a>
+
+        <a
+          href="https://csc.gov.in"
+          target="_blank"
+          rel="noreferrer"
+        >
+          CSC India Portal
+        </a>
       </div>
-<div className="developer-credit">
-  <p>Designed & Developed by</p>
-  <h4>Anish Garg</h4>
-  <span>Software Developer</span>
-</div>
+
+      <div className="developer-credit">
+        <p>Designed & Developed by</p>
+        <h4>Anish Garg</h4>
+        <span>Software Developer</span>
+      </div>
+
       <div className="footer-bottom">
-        © {new Date().getFullYear()} Garg Common Service Center | Faridabad, Haryana &nbsp;|&nbsp; Powered by Digital India 🇮🇳
-        
+        © {new Date().getFullYear()} Garg Common Service Center |
+        Faridabad, Haryana | Powered by Digital India 🇮🇳
       </div>
     </footer>
   )
 }
-<div className="developer-credit">
-  <p>Designed & Developed by</p>
-  <h4>Anish Garg</h4>
-  <span>Software Developer</span>
-</div>
